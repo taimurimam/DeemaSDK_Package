1e3079b20f2c2341fddb5ff4d8791ab01ef28bbf
Taimur Imam <taimurimam@gmail.com>
2024-12-10 13:56:07 +0530

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DeemaSDK_Package",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DeemaSDK_Package",
+            targets: ["DeemaSDK_Package"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "DeemaSDK_Package"),
+        .testTarget(
+            name: "DeemaSDK_PackageTests",
+            dependencies: ["DeemaSDK_Package"]
+        ),
+    ]
+)
