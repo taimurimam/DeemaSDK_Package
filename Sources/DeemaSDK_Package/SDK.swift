430f93024f75adfafeed6a6df6c876bd9c781a68
Taimur Imam <taimurimam@gmail.com>
2024-12-10 14:15:23 +0530

    2nd post

--- Sources/DeemaSDK_Package/SDK.swift
+++ Sources/DeemaSDK_Package/SDK.swift
@@ -8,7 +8,15 @@
 
 import SwiftUI
 
+ 
+
 public struct SDK : View {
+    public init(buttonTapped: @escaping () -> Void) {
+        self.buttonTapped = buttonTapped
+    }
+    
+    let buttonTapped: ()->Void
+
     public var body: some View {
         Text("I am sdk")
             .background(Color.white)
