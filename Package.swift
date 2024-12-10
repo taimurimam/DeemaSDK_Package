bdacbebdbe214e165787aabf4be230c8fd2fe242
Taimur Imam <taimurimam@gmail.com>
2024-12-10 14:07:32 +0530

    1st push

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "DeemaSDK_Package",
+    platforms: [
+        .iOS(.v16)
+    ] ,
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,6 +15,7 @@ let package = Package(
             targets: ["DeemaSDK_Package"]),
     ],
     targets: [
+        
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
---
+++ Sources/DeemaSDK_Package/SDK.swift
@@ -0,0 +1,16 @@
+//
+//  SDK.swift
+//  DeemaSDK_Package
+//
+//  Created by Taimur Imam on 10/12/24.
+//
+
+
+import SwiftUI
+
+public struct SDK : View {
+    public var body: some View {
+        Text("I am sdk")
+            .background(Color.white)
+    }
+}
